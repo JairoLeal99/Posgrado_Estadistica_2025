51dc8e5b98d05c01ebc2474bb6a1486d7551d085
JAIRO_POSGRADO <jairoleal9915@gmail.com>
2025-08-14 10:10:41 -0600

    3

--- Laboratorio 1.R
+++ Laboratorio 1.R
@@ -1,6 +1,55 @@
 # LABORATORIO 1 - (14/08/2025)
 
-+ Sumas
+# Sumas
 300+240+1527+400+1500+1833
 
-+ 
+# Crear objetos
+celular <- 300
+celular
+transporte <- 240
+comestibles <- 1527
+gimnasio <- 400
+alquiler <- 1500
+otro <- 1833
+
+celular + transporte + comestibles + gimnasio + alquiler + otro
+
+gastomes <- (celular + transporte + comestibles + gimnasio + alquiler + otro)
+gastomes
+gastomes*5
+gastomes*2
+
+# Reglas de nombre objetos
+
+"yo_uso_guion_bajo
+otras.personas.usan.punto
+OtrosUsanMayusculas"
+
+# Funciones
+
+abs(10)
+abs(-4)
+sqrt(9)
+log(2)
+
+# Comentarios
+
+# Simbolo hashtag #
+
+# Este es un comentario 
+
+# Este es otro comentario
+
+
+
+
+
+
+
+
+
+
+
+
+
+
